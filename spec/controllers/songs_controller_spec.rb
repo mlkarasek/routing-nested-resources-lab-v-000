578fa1c296d7542253f28275498b4a233cdfc91e
Monica  Karasek <mlkarasek@gmail.com>
2018-08-29 23:40:04 +0000

    automatically backed up by learn

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -32,7 +32,6 @@ RSpec.describe SongsController do
 
     it "redirects to artists songs when artist song not found" do
       get :show, id: 12345, artist_id: @artist.id
-      expect(controller).to set_flash[:alert]
       expect(response).to redirect_to artist_songs_path(@artist)
     end
 
